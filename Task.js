613752384a20c202f31acf5a64773ea5b5ffe64a
Avinash Singh <asingh@1worldsync.com>
2021-11-09 20:12:28 +0530

    Task

--- Task.js
+++ Task.js
@@ -1,1 +1,13 @@
-var a= 1;
+//reduce
+const eu = [1, 2, 3, 4];
+
+const doubled = eu.reduce((total, amount) => {
+  total.push(amount * 2);
+  return total;
+}, []);
+console.log(doubled);
+
+//map
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(item => item * 2);
+console.log(doubled);
